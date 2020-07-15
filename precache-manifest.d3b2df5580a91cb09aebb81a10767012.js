48a95172cd51bc758f0554fa28498b9f6e6e25b2
chenmal <chenmalish@gmail.com>
2020-07-15 17:15:44 +0300

    Updates

---
+++ precache-manifest.d3b2df5580a91cb09aebb81a10767012.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0de2bb8a956b5da32a7b0618819cba79",
+    "url": "/GetIt/index.html"
+  },
+  {
+    "revision": "cc3fd1cc9df67e096dc1",
+    "url": "/GetIt/static/js/2.1481d819.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/GetIt/static/js/2.1481d819.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f57e17bfb1444c1ecfe",
+    "url": "/GetIt/static/js/main.afd8e9bf.chunk.js"
+  },
+  {
+    "revision": "7c53ffb24375cd8e4d1d",
+    "url": "/GetIt/static/js/runtime-main.91629970.js"
+  },
+  {
+    "revision": "72802ed4b4de038603d3b621e32d3db9",
+    "url": "/GetIt/static/media/8531.72802ed4.jpg"
+  },
+  {
+    "revision": "1cfbf8d11a2277b1441957e83056d505",
+    "url": "/GetIt/static/media/background.1cfbf8d1.jpg"
+  },
+  {
+    "revision": "d0ebaf0ea689d48210f4be2619472f73",
+    "url": "/GetIt/static/media/logo2.d0ebaf0e.jpg"
+  }
+]);
